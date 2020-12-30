cd1b0477ccda5a3b6edebbc52c15c0391434f079
Ramsy de Vos <ramsy@ramsy.dev>
2020-12-30 18:58:38 +0100

    feat(data): update http-client

--- core/data/protocols/http/http-client.ts
+++ core/data/protocols/http/http-client.ts
@@ -1,12 +1,16 @@
 export interface HttpClientSpec {
     post<P>(path: string, payload: HttpClient.Post): Promise<P>;
+    get<G>(path: string, payload?: HttpClient.Get): Promise<G>;
+    delete<D>(path: string, payload?: HttpClient.Delete): Promise<D>;
 }
 
-export abstract class HttpClient implements HttpClientSpec {
+export abstract class HttpClient {
 
     constructor(public baseUrl?: string) {}
 
     abstract post<P>(path: string, payload: HttpClient.Post): Promise<P>;
+    abstract get<G>(path: string, payload?: HttpClient.Get): Promise<G>;
+    abstract delete<D>(path: string, payload?: HttpClient.Delete): Promise<D>;
 }
 
 export declare namespace HttpClient {
@@ -20,4 +24,6 @@ export declare namespace HttpClient {
     export interface Post extends Common {
         body?: any;
     }
+    export interface Get extends Common {}
+    export interface Delete extends Common {}
 }
